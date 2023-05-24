fb4f304ed1f33462078f84d9b04012dc0db8e4d0
Barışcan Sargın <bariscansargin4@gmail.com>
2023-05-24 16:58:13 +0300

    Added Navbar component.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between items-center h-24 p-16 ">
+      <div>
+        <NavLink to={"/"}>
+          <h1 className="text-3xl  font-extrabold  tracking-[1rem] text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+            MAKE COCTAIL
+          </h1>
+        </NavLink>
+      </div>
+
+      <nav>
+        <ul>
+          <NavLink className="m-12 tracking-wide">Home</NavLink>
+          <NavLink className="m-12 tracking-wide">Coctails</NavLink>
+          <NavLink className="m-12 tracking-wide">Ingredients</NavLink>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
