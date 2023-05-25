4ec29bfea3408369b56362311ee28693bb493c0f
Barışcan Sargın <bariscansargin4@gmail.com>
2023-05-25 17:58:26 +0300

    Created Error Page.

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Navbar from "../Navbar/Navbar";
+
+const ErrorPage = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <p>Düzenle burayı page error !</p>
+    </Fragment>
+  );
+};
+
+export default ErrorPage;
