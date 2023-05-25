9a827e170f283f8d8a404949f2740e7ad3fbc85b
Barışcan Sargın <bariscansargin4@gmail.com>
2023-05-25 12:51:05 +0300

    Created Small Coctail Card Component.

---
+++ src/components/SmallCocktailCard/SmallCocktailCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ButtonComponent from "../Button/ButtonComponent";
+
+const SmallCocktailCard = ({ cocktail, info, clickHandler }) => {
+  return (
+    <div className="flex flex-col items-center justify-center shadow-lg border-solid rounded-xl mb-12 bg-[#B0DAB9] p-12">
+      <p className="mb-4"> Have you ever tried ? </p>
+      <img
+        className="w-64 h-64 rounded-2xl mb-4 "
+        src={cocktail.strDrinkThumb}
+        alt="cocktail-photo"
+      />
+      <p className="mb-4">{cocktail.strDrink}</p>
+      <div className="flex mt-4 ">
+        <Link>
+          <ButtonComponent
+            type={"green"}
+            position={"w-12"}
+            clickHandler={clickHandler}
+          >
+            &#x2713;
+          </ButtonComponent>
+        </Link>
+        <Link>
+          <ButtonComponent type={"red"} position={"ml-8  w-12 "}>
+            &times;
+          </ButtonComponent>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SmallCocktailCard;
