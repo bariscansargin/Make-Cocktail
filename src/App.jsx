1b4adefa9b4726053f2316ad7589a53527914f45
Barışcan Sargın <bariscansargin4@gmail.com>
2023-05-26 15:50:35 +0300

    Add styling.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PageLayout from "./pages/HomePageLayout/HomePageLayout";
 import MainPage from "./pages/MainPage/MainPage";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import SingleCoctailPage from "./pages/SingleCoctailPage/SingleCoctailPage";
+import SingleIngredientPage from "./pages/SingleIngredientPage/SingleIngredientPage";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <MainPage /> },
       { path: "/cocktails/:cocktailId", element: <SingleCoctailPage /> },
+      {
+        path: "/ingredients/:ingredientName",
+        element: <SingleIngredientPage />,
+      },
     ],
   },
 ]);
--- src/pages/SingleCoctailPage/SingleCoctailPage.jsx
+++ src/pages/SingleCoctailPage/SingleCoctailPage.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 
 const SingleCoctailPage = () => {
-  const [ingredients, setIngredients] = useState([]);
   const { cocktailId } = useParams();
+
   async function fetchCocktailById() {
     try {
       const res = await axios.get(
@@ -19,16 +20,16 @@ const SingleCoctailPage = () => {
 
   function findIngredients(data) {
     const ingredients = Object.keys(data).reduce((acc, key) => {
-      if (key.includes('strIngredient') && data[key]) {
-          const index = key.split('strIngredient')[1];
-          acc.push({
-              ingredient: data[key],
-              measure: data[`strMeasure${index}`],
-          });
+      if (key.includes("strIngredient") && data[key]) {
+        const index = key.split("strIngredient")[1];
+        acc.push({
+          ingredient: data[key],
+          measure: data[`strMeasure${index}`],
+        });
       }
-      return acc;
-  }, [])
-    return ingredients; 
+      return acc;
+    }, []);
+    return ingredients;
   }
 
   const cocktailQueryById = useQuery({
@@ -36,20 +37,51 @@ const SingleCoctailPage = () => {
     queryFn: fetchCocktailById,
   });
   return (
-    <main>
-      {console.log(cocktailQueryById)}
+    <main className="flex flex-col items-center justify-center text-center mt-4">
       {cocktailQueryById.isLoading && <p className="text-center">Loading...</p>}
       {cocktailQueryById.data && (
-        <div>
-          <p>{cocktailQueryById.data.drinks[0].strDrink}</p>
+        <div className="flex flex-col items-center justify-center">
+          <p className="mb-4 text-pink-700 text-2xl italic">
+            {cocktailQueryById.data.drinks[0].strDrink}
+          </p>
           <img
             src={cocktailQueryById.data.drinks[0].strDrinkThumb}
             alt="cocktail-photo"
+            className="w-64 h-64 rounded-xl mb-4 hover:animate-spin"
           />
-          <p>{cocktailQueryById.data.drinks[0].strCategory}</p>
+          <p className="text-center text-xl text-pink-700 italic mb-4">
+            {cocktailQueryById.data.drinks[0].strCategory}
+          </p>
 
-          <p>{cocktailQueryById.data.drinks[0].strInstructions}</p>
-          {console.log(findIngredients(cocktailQueryById.data.drinks[0]))}
+          <p className="text-l text-pink-900 mb-4">
+            {cocktailQueryById.data.drinks[0].strInstructions}
+          </p>
+          <div className="flex flex-col items-center justify-around p-12 ">
+            <div className="flex justify-between w-[500px] mb-4">
+              <p className="bg-pink-700 opacity-1 text-white rounded-xl p-1.5">
+                Ingredients
+              </p>
+              <p className="bg-pink-700 opacity-1 text-white rounded-xl p-1.5">
+                Measures
+              </p>
+            </div>
+
+            {findIngredients(cocktailQueryById.data.drinks[0]) &&
+              findIngredients(cocktailQueryById.data.drinks[0]).map(
+                (data, idx) => {
+                  return (
+                    <div className="flex justify-between w-[500px] " key={idx}>
+                      <Link to={`/ingredients/${data.ingredient}`}>
+                        <p className="text-lg ml-2 hover:text-pink-700">
+                          {data.ingredient}
+                        </p>
+                      </Link>
+                      <p className="italic text-lg mr-2">{data.measure}</p>
+                    </div>
+                  );
+                }
+              )}
+          </div>
         </div>
       )}
     </main>
