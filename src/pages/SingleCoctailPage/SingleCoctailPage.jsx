8fb4e034833d77350abca669ff8f9f6408733b92
Barışcan Sargın <bariscansargin4@gmail.com>
2023-05-30 11:50:45 +0300

    Update Single Cocktail Page.

--- src/pages/SingleCoctailPage/SingleCoctailPage.jsx
+++ src/pages/SingleCoctailPage/SingleCoctailPage.jsx
@@ -37,12 +37,13 @@ const SingleCoctailPage = () => {
     queryFn: fetchCocktailById,
   });
   return (
-    <main className="flex flex-col items-center justify-center text-center mt-4">
+    <main className="flex flex-grow flex-col items-center justify-center text-center mt-4">
       {cocktailQueryById.isLoading && <p className="text-center">Loading...</p>}
       {cocktailQueryById.data && (
         <div className="flex flex-col items-center justify-center">
           <p className="mb-4 text-pink-700 text-2xl italic">
-            {cocktailQueryById.data.drinks[0].strDrink}
+            {cocktailQueryById.data.drinks[0].strDrink}(
+            {cocktailQueryById.data.drinks[0].strAlcoholic})
           </p>
           <img
             src={cocktailQueryById.data.drinks[0].strDrinkThumb}
@@ -52,10 +53,12 @@ const SingleCoctailPage = () => {
           <p className="text-center text-xl text-pink-700 italic mb-4">
             {cocktailQueryById.data.drinks[0].strCategory}
           </p>
+          <div className="flex items-center justify-center px-64 ">
+            <p className="text-l text-pink-900 mb-4 text-center">
+              {cocktailQueryById.data.drinks[0].strInstructions}
+            </p>
+          </div>
 
-          <p className="text-l text-pink-900 mb-4">
-            {cocktailQueryById.data.drinks[0].strInstructions}
-          </p>
           <div className="flex flex-col items-center justify-around p-12 ">
             <div className="flex justify-between w-[500px] mb-4">
               <p className="bg-pink-700 opacity-1 text-white rounded-xl p-1.5">
