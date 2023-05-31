3dfcebec857c467b8705915e530dac29683e1382
Barışcan Sargın <bariscansargin4@gmail.com>
2023-05-31 10:28:48 +0300

    Update CocktailLinkCard.jsx

--- src/components/CocktailLink/CocktailLinkCard.jsx
+++ src/components/CocktailLink/CocktailLinkCard.jsx
@@ -6,11 +6,11 @@ const CocktailLinkCard = ({ cocktail }) => {
   return (
     <Link
       to={`/cocktails/${cocktail.idDrink}`}
-      className="flex flex-col bg-cyan-500 hover:bg-pink-300 hover:shadow-pink-300 hover:scale-110 transition duration-150  ease-in items-center justify-center w-48 h-128 mb-12 shadow-2xl shadow-cyan-500 rounded-lg md:mr-8"
+      className="flex flex-col  bg-[#B0DAB9] hover:bg-cyan-500 hover:shadow-cyan-500 hover:scale-110 transition duration-150  ease-in items-center justify-center w-48 h-128 mb-12 shadow-2xl shadow-gray-400 rounded-lg md:mr-8"
     >
       <img src={cocktail.strDrinkThumb + "/preview"} className="rounded-t-lg" />
       <div className="p-5 h-28 flex justify-center items-center ">
-        <h1 className="text-center text-white ">
+        <h1 className="text-center text-green-900 ">
           {cocktail.strDrink.length < 30
             ? cocktail.strDrink
             : truncatedText(cocktail.strDrink) + "..."}
